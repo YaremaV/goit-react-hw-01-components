3157e3d634ae9589e05f11aa3e0c6b68757564c9
YaremaV <Yaremenko1996v@gmail.com>
2021-09-28 21:46:43 +0300

    Add .prettier.json and formated files

--- src/components/statistics/statistics.js
+++ src/components/statistics/statistics.js
@@ -1,17 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
-import './statistics.css'
+import s from './statistics.module.css'
 
 
 export default function Statistics({stats, title}){
-    return <section className="statistics">
-        {title && <h2 className={title}>{title}</h2>}
-   <ul className="stat-list">
+    return <section className={s.statistics}>
+        {title && <h2 className={s.title}>{title}</h2>}
+   <ul className={s.list}>
  {stats.map(stat => (
-   <li key={stat.id} className="item"
+   <li key={stat.id} className={s.item}
    style={{backgroundColor: stat.color}}>
-        <span className="label">{stat.label}</span>
-      <span className="percentage">{stat.percentage}%</span>
+        <span className={s.label}>{stat.label}</span>
+      <span className={s.percentage}>{stat.percentage}%</span>
     </li>
     ))}
   </ul>
